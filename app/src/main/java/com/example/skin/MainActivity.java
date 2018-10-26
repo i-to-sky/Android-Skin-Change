aed66f47e5a203705c4f49dc2f844974a9a06cba
weiyupei <weiyupei@xiaomi.com>
2018-10-26 15:40:34 +0800

    增加对ListView等动态创建内容的View的换肤测试

--- app/src/main/java/com/example/skin/MainActivity.java
+++ app/src/main/java/com/example/skin/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.skin;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.example.i_to_sky.skin_library.activity.BaseActivity;
 import com.example.i_to_sky.skin_library.manager.SkinManager;
@@ -19,6 +24,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     Button mResetSkinBtn;
     Button mGreenSkinBtn;
     Button mBlueSkinBtn;
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,70 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mGreenSkinBtn.setOnClickListener(this);
         mBlueSkinBtn = (Button) findViewById(R.id.change_skin_blue_btn);
         mBlueSkinBtn.setOnClickListener(this);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_list_view, null);
+                updateView(convertView);
+                return convertView;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 1;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
 
     }
 
